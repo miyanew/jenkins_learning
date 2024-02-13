d8b9fd46a28b79254880771cd230f327256ebe01
miyanew <dev38_shin@yahoo.co.jp>
2024-02-13 22:04:52 +0900

    fix error agent

--- build_ruby.groovy
+++ build_ruby.groovy
@@ -1,11 +1,14 @@
 pipeline {
-    agent { docker { image 'ruby:3.3.0-alpine3.19' } }
+    agent any
     stages {
         stage('build') {
             steps {
-                sh 'ruby --version'
+                script {
+                    docker.image('ruby:3.3.0-alpine3.19').inside {
+                        sh 'ruby --version'
+                    }
+                }
             }
         }
     }
 }
-
