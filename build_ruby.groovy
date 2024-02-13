032d421e01dd9d35d2b36cf648e86a6c420519ad
miyanew <dev38_shin@yahoo.co.jp>
2024-02-13 22:17:05 +0900

    fix error

--- build_ruby.groovy
+++ build_ruby.groovy
@@ -4,10 +4,11 @@ pipeline {
         stage('build') {
             steps {
                 sh 'ruby --version'
-                sh 'which docker'
                 sh 'uname -a;id;pwd'
                 sh 'ls -l'
-                    }
+                sh 'docker -v'
+                sh 'which which'
             }
         }
     }
+}
